7a0e42e023cf7d9f55be605445528191635fbd5f
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-21 11:36:40 +0600

    created the routes and connected with app

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 const authRoutes = require("../modules/auth/auth.route");
 
+const successorRoutes = require("../modules/successor/successor.route");
+
 router.use("/auth", authRoutes);
+
+router.use("/successors", successorRoutes);
 
 module.exports = router;
