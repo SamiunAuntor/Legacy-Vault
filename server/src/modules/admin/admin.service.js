993344e7fb617a1a35802a9703c7773a4cbe417c
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-24 00:26:55 +0600

    created the admin services

---
+++ server/src/modules/admin/admin.service.js
@@ -0,0 +1,32 @@
+const User = require("../user/user.model");
+
+const Claim = require("../claim/claim.model");
+
+const getDashboardStats = async () => {
+    const totalUsers =
+        await User.countDocuments();
+
+    const totalClaims =
+        await Claim.countDocuments();
+
+    const pendingClaims =
+        await Claim.countDocuments({
+            status: "UNDER_REVIEW",
+        });
+
+    const approvedClaims =
+        await Claim.countDocuments({
+            status: "APPROVED",
+        });
+
+    return {
+        totalUsers,
+        totalClaims,
+        pendingClaims,
+        approvedClaims,
+    };
+};
+
+module.exports = {
+    getDashboardStats,
+};
